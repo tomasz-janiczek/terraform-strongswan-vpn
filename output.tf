a0f90ed0ac816179be6f841828294106cf9333bc
Tomasz Janiczek <tomasz.janiczek@tsh.io>
2022-01-29 20:14:34 +0100

    Added some descriptions to the outputs

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 
 output "vpn_instance_id" {
-  value     = aws_instance.vpn_server.id
+  value       = aws_instance.vpn_server.id
+  description = "The ID of the EC2 instance that hosts the strongSwan VPN"
 }
